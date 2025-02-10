236b5192657e63f06dd2f2f913ecf62279c26166
João Almeida <toxicyszconta@gmail.com>
2025-02-10 10:53:34 -0400

    somar

--- contador.py
+++ contador.py
@@ -8,18 +8,32 @@ Window.size = (360, 640)
 class MeuApicativo(App):
     def build(self):
         box_main = BoxLayout(orientation="vertical")
-        texto = TextInput(text="1")
-        botaozao = Button(text="aaaa", on_press=self.contar)
+        self.texto = TextInput(text="1")
+        self.botaozao = Button(text="+", on_press=self.contar)
         # botaozao.bind(on_press = self.contar)
 
+        self.botaozinho = Button(text="-", on_press=self.diminuir)
 
-        box_main.add_widget(texto)
-        box_main.add_widget(botaozao)
+
+        box_main.add_widget(self.texto)
+        box_main.add_widget(self.botaozao)
+        box_main.add_widget(self.botaozinho)
 
         return box_main
     
     def contar(self, instance):
-        x = 1
-        print(instance)
+        num = 1
+
+        num += int(self.texto.text)
+        self.texto.text = str(num)
+        print(num)
+
+    def diminuir(self, instance):
+        num = 1
+
+        num = int(self.texto.text) - 1
+        self.texto.text = str(num)
+        print(num)
+
 
 MeuApicativo().run()
